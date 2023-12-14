97cb7486407dd928fb6e14c2986129846e76bce2
Jeffrey <jeffcolinchu@gmail.com>
2023-12-14 14:55:32 +0800

    Updated Weathercard component to mount properly

--- weathergram/src/components/Weathercard.jsx
+++ weathergram/src/components/Weathercard.jsx
@@ -1,17 +1,25 @@
 const Weathercard = ({ onData }) => {
-    if (onData) {
-        return <div>Loading...</div>
+    //Todo: how to render and mount this component if onData is initially {}
+    //* why does onData == {} not work for conditional rendering? Is an empty {} actually being passed to this componenet?
+    //*Am i not allowed to define destructured variables from objects that are empty?
+    //*I have to destrucutre props when passing between child and parent, why?
+    
+    if (!onData?.name) {
+        return <div></div>
     }
+
     const { name } = onData;
     const { icon, description } = onData.weather[0];
     const { temp, humidity } = onData.main;
     const { speed } = onData.wind;
     console.log(name, icon, description, temp, humidity, speed);
 
+    
+
     return (
         <div className="bg-[#000000d0] text-white p-8 rounded-[24px] w-full max-w-md mx-4">
             <div className="">
-                <h2>Weather in {name}</h2>
+                <h2>{`Weather in ${name}`}</h2>
                 <h1 className="m-0 mb-[0.4em]">{`${Math.round(temp)}°C`}</h1>
                 <div className="">
                     <img
