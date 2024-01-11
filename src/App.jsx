c37252d62c99e22b414fa5010d1114b23372a8df
Jeffrey <jeffcolinchu@gmail.com>
2024-01-11 17:19:46 -0500

    Fixed CSS of the overall layout

--- src/App.jsx
+++ src/App.jsx
@@ -12,12 +12,18 @@ function App() {
 
     return (
         <>
-            <div className=" px-5 flex flex-col justify-center items-center h-screen m-0 bg-[url('https://source.unsplash.com/1669x931/?landscape')] bg-cover">
-                <Searchbar onSetData={setData} onSetHistory={setHistory} onHistory={history} />
-                <Quotecard onData={data} />
-                <div className="flex flex-row" >
-                    <Weathercard onData={data} />
-                    <Historycard onHistory={history} onSetHistory={setHistory} onData={data} />
+            <div className="bg-[url('https://source.unsplash.com/1669x931/?landscape')] bg-cover flex flex-col h-screen justify-center">
+                <div className="flex flex-col">
+                        <div className="px-5 flex justify-center">
+                            <Searchbar onSetData={setData} onSetHistory={setHistory} onHistory={history} />
+                        </div>
+                    <div className=" px-5 flex justify-center items-center">
+                        <Quotecard onData={data} />
+                        <div className="flex flex-row " >
+                            <Weathercard onData={data} className=""/>
+                            <Historycard onHistory={history} onSetHistory={setHistory} onData={data} className="" />
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
--- src/components/Historycard.jsx
+++ src/components/Historycard.jsx
@@ -10,12 +10,12 @@ const Historycard = ({onHistory, onSetHistory, onData}) => {
     }
 
     return (
-        <div className=" bg-[#000000d0] text-white p-8 rounded-[24px] w-full max-w-sm mx-4">
+        <div className=" bg-[#000000d0] text-white p-8 rounded-[24px] w-full max-w-sm mx-4 relative ">
             <h2 className="mb-1">Weather log</h2>
-            <ul>
-                {onHistory.map(item => <div key={item.id} > <li> {item.name} {Math.round(item.main.temp)}°C </li> </div>)}
+            <ul className="max-h-80 overflow-y-auto mb-9" >
+                {onHistory.map(item => <div className="" key={item.id} > <li> {item.name} {Math.round(item.main.temp)}°C </li> </div>)}
             </ul>
-            <button className="" onClick={onClear}>Clear</button>
+            <button className=" mt-1 absolute bottom-5 left-5 bg-blue-500 text-white px-4 py-2 rounded-lg" onClick={onClear}>Clear</button>
         </div>
     );
 };
