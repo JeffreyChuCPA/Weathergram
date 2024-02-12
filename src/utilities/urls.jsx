1e19bb0eeaf95fd2292a5601ec8275b21ccdeded
Jeffrey Chu <jeffcolinchu@gmail.com>
2024-02-12 12:42:26 -0500

    Config project for deployment

--- src/utilities/urls.jsx
+++ src/utilities/urls.jsx
@@ -1,6 +1,6 @@
 const urls = {
   weatherURL: 'https://api.openweathermap.org/data/2.5',
-  quoteURL: 'https://api.quotable.io/random'
+  quoteURL: 'http://api.quotable.io/random'
 }
 
 export default urls
