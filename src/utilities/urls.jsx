cf2ea03fea560798298131e28ccac96a23d45a7d
Jeffrey Chu <141854177+JeffreyChuCPA@users.noreply.github.com>
2024-03-27 17:42:00 -0400

    Update urls.jsx for HTTPS protocol

--- src/utilities/urls.jsx
+++ src/utilities/urls.jsx
@@ -1,6 +1,6 @@
 const urls = {
   weatherURL: 'https://api.openweathermap.org/data/2.5',
-  quoteURL: 'http://api.quotable.io/random'
+  quoteURL: 'https://api.quotable.io/random'
 }
 
 export default urls
